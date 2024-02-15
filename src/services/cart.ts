132a7317c986e42d30cc19bc6199b724d27b1a50
j <1336679971@qq.com>
2024-02-15 17:36:43 +0800

    cart模块

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -21,3 +21,43 @@ export const getMemberCartAPI = () => {
     url: '/member/cart',
   })
 }
+
+/**
+ * 删除/清空购物车单品
+ * @param data 请求体参数 ids SKUID 集合
+ */
+export const deleteMemberCartAPI = (data: { ids: string[] }) => {
+  return http({
+    method: 'DELETE',
+    url: '/member/cart',
+    data,
+  })
+}
+
+/**
+ * 修改购物车单品
+ * @param skuId SKUID
+ * @param data selected 选中状态 count 商品数量
+ */
+export const putMemberCartBySkuIdAPI = (
+  skuId: string,
+  data: { selected?: boolean; count?: number },
+) => {
+  return http({
+    method: 'PUT',
+    url: `/member/cart/${skuId}`,
+    data,
+  })
+}
+
+/**
+ * 购物车全选/取消全选
+ * @param data selected 是否选中
+ */
+export const putMemberCartSelectedAPI = (data: { selected: boolean }) => {
+  return http({
+    method: 'PUT',
+    url: '/member/cart/selected',
+    data,
+  })
+}
